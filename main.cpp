415a2456cfccf1d8829dfb1a58c5845bd4bf4a9b
sarvin <sraeis2@myseneca.ca>
2022-09-10 22:41:48 -0400

    init commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/* workshop1
+ * name: Sarvin Raeisi
+ * ID: 163927197
+ * email: sraeisi2@myseneca.ca
+ * course: BTH545NAA
+**/
+#include <QApplication>
+#include <QPushButton>
+//#include <QPalette>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QPushButton button("Hello BTH545");
+    button.resize(150,35);
+
+    //setting background color to the button
+    button.setStyleSheet("background-color : orange");
+
+    //setting color to button's borders
+    /*QPalette pal = button.palette();
+    pal.setColor(QPalette::Button, QColor(Qt::magenta));
+    button.setAutoFillBackground(true);
+    button.setPalette(pal);
+    button.update();*/
+
+
+
+    button.show();
+    return a.exec();
+}
